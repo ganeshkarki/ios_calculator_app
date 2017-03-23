43b4d654828ede826bb23b690eb53e20e5a521cb
Ganesh Karki <ganeshkarkigk@gmail.com>
2017-03-23 16:26:39 +0900

    Main Locgic for calculator

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,77 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Karki, Ganesh | Ganesh | OSPD on 3/22/17.
+//  Copyright © 2017 Karki, Ganesh | Ganesh | OSPD. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    typealias PropertyList = AnyObject
+    
+    private var accumulator = 0.0
+    func setOperand(operand: Double){
+        accumulator = operand
+    }
+    
+    var operations: Dictionary<String,Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "sin" : Operation.UnaryOperation(sin),
+        "cos" : Operation.UnaryOperation(cos),
+        "+" : Operation.BinaryOperation({$0 + $1}),
+        "-" : Operation.BinaryOperation({$0 - $1}),
+        "÷" : Operation.BinaryOperation({$0 / $1}),
+        "×" : Operation.BinaryOperation({$0 * $1}),
+        "=" : Operation.Equals
+        ]
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double)->Double)
+        case BinaryOperation((Double, Double)->Double)
+        case Equals
+    }
+    
+    struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol]{
+            switch operation{
+                case .Constant(let value):
+                    accumulator = value
+                case .UnaryOperation(let function):
+                    accumulator = function(accumulator)
+                case .BinaryOperation(let function):
+                    pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+                case .Equals:
+                    executePendingBinaryOperation()
+            }
+        }
+        
+    }
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    var  result: Double{
+        get {
+            return accumulator
+        }
+    }
+    
+    func isDecimalNotPresent(value: String) -> Bool {
+        return value.rangeOfString(String(".")) == nil
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Karki, Ganesh | Ganesh | OSPD on 3/21/17.
+//  Copyright © 2017 Karki, Ganesh | Ganesh | OSPD. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    let space = String(" ");
+    
+    var userInMiddleOfTyping = false
+    var decimalEntered = false
+    var expressionFinished = false
+    
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var history: UILabel!
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        if expressionFinished{ history.text = nil ; expressionFinished = false}
+        let digit = sender.currentTitle!
+        if userInMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text
+            display.text = textCurrentlyInDisplay! + digit
+        }else{
+            display.text = digit
+        }
+        userInMiddleOfTyping = true
+    }
+    
+    var displayValue: Double{
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    
+    
+    @IBAction func performOperation(sender: UIButton) {
+        if userInMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userInMiddleOfTyping = false
+        }
+        var mathematicalSymbol: String!
+        mathematicalSymbol = sender.currentTitle
+        brain.performOperation(mathematicalSymbol)
+        
+        
+        if expressionFinished{ history.text = nil ; expressionFinished = false}
+        if let hist = history.text {
+            history.text = hist + space + String(displayValue) + space + mathematicalSymbol
+        } else {
+            history.text =  String(displayValue) + space + mathematicalSymbol
+        }
+        
+        if mathematicalSymbol == String("=") {
+            history.text = history.text! + space + String(brain.result)
+            expressionFinished = true
+        }
+        
+        displayValue = brain.result
+
+    }
+    
+    @IBAction func touchDecimal(sender: UIButton) {
+        
+        if let textCurrentlyInDisplay = display.text {
+            if brain.isDecimalNotPresent(textCurrentlyInDisplay){
+                display.text = textCurrentlyInDisplay + String(".")
+            }
+        } else {
+            display.text = String("0.")
+        }
+        decimalEntered = true
+    }
+    
+    @IBAction func clearHistory(sender: UIButton) {
+        history.text = String("")
+        display.text = String("0")
+    }
+    
+}
+
